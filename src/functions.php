cb451252dcff99a4e93b1083d7785d6235ab59a8
Kevin Bond <kevinbond@gmail.com>
2024-01-08 19:22:34 -0500

    minor: optimize creating array collection via `collect()`

--- src/functions.php
+++ src/functions.php
@@ -12,6 +12,7 @@
 namespace Zenstruck;
 
 use Doctrine\Common\Collections\Collection as DoctrineCollection;
+use Zenstruck\Collection\ArrayCollection;
 use Zenstruck\Collection\Doctrine\DoctrineBridgeCollection;
 use Zenstruck\Collection\LazyCollection;
 
@@ -21,7 +22,7 @@ use Zenstruck\Collection\LazyCollection;
  *
  * @param null|iterable<K,V>|callable():iterable<K,V> $source
  *
- * @return LazyCollection<K,V>
+ * @return Collection<K,V>
  * @phpstan-return ($source is null ? Collection<never,never> : Collection<K,V>)
  */
 function collect(iterable|callable|null $source = null): Collection
@@ -32,6 +33,10 @@ function collect(iterable|callable|null $source = null): Collection
 
     if ($source instanceof DoctrineCollection) {
         return new DoctrineBridgeCollection($source);
+    }
+
+    if (\is_array($source)) {
+        return new ArrayCollection($source);
     }
 
     return new LazyCollection($source ?? []);
